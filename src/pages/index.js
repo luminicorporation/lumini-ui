66a9c25b66bc4e98dac2345b58b47dbcfd57e6d4
Pablo Garcia <pgarciacamou@gmail.com>
2019-03-12 16:47:00 -0700

    add anchors

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,6 +7,7 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 import Content from '../components/content';
 import '../styles/index.scss';
+import { Anchor } from '../components/common/anchor';
 
 const NoWrap = p => <span style={{ whiteSpace: 'nowrap' }}>{p.children}</span>;
 
@@ -42,7 +43,10 @@ const IndexPage = () => (
             Mobile sensors, Artificial intelligence, Cloud automation, Machine
             learning, Neural networks, and more.
           </p>
-          <p>Learn more about Lumini Research: info@lumini.me</p>
+          <p>
+            Learn more about Lumini Research:{' '}
+            <Anchor href="mailto:info@lumini.me">info@lumini.me</Anchor>
+          </p>
         </div>
       </article>
     </Content>
