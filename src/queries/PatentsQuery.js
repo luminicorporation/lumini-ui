9c907bf6e41f4a5b720d543672214a994223bada
Pablo Garcia <pgarciacamou@gmail.com>
2019-04-17 13:24:40 -0700

    add PatentsQuery using gatsby-transformer-json gatsby-source-filesystem

---
+++ src/queries/PatentsQuery.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { StaticQuery, graphql } from 'gatsby';
+
+// Returns patents obtained from `src/data/patents.json` and appends the srcSet
+// of the images obtained after parsing `src/images/patents/*`
+export const PatentsQuery = ({ render }) => {
+  return (
+    <StaticQuery
+      query={graphql`
+        query PatentsQuery {
+          allPatentsJson {
+            edges {
+              node {
+                id
+                name
+                href
+                image {
+                  name
+                  src {
+                    childImageSharp {
+                      fluid {
+                        srcSet
+                      }
+                    }
+                  }
+                }
+              }
+            }
+          }
+        }
+      `}
+      render={render}
+    />
+  );
+};
